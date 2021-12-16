564ab8760dfe52cdc00902c097b4678d4189e6e1
cutegentle <1260766599@qq.com>
2021-12-16 15:49:21 +0800

    Site updated: 2021-12-16 15:49:20

--- js/music/music.js
+++ js/music/music.js
@@ -5,9 +5,9 @@ const ap = new APlayer({
 	autoplay: true, //自动播放
     audio: [
 	{
-        name: 'Talking to the Moon',
-        artist: 'Bruno Mars',
-        url: 'http://m10.music.126.net/20211216144917/d8a49d27e27d504249ea03b7ccb6b62b/ymusic/3bac/8775/44f4/2bf1a676bef1b8470232df384a42a71f.mp3',
+        name: 'moon teller',
+        artist: 'Jason Barthes',
+        url: 'http://music.163.com/song/media/outer/url?id=1431705653.mp3',
         cover: 'https://p1.music.126.net/ZSNq_4FdLRKb0eEic_4Obw==/109951163691583679.jpg?param=130y130',
     }, 
 	]
