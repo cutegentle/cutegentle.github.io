27e918556e4221ee4c0d66f11ab777d2a02f9b53
cutegentle <1260766599@qq.com>
2021-12-17 10:19:26 +0800

    Site updated: 2021-12-17 10:19:25

--- js/duration.js
+++ js/duration.js
@@ -2,7 +2,7 @@
 
 !(function() {
   /** 计时起始时间，自行修改 **/
-  var start = new Date("2020/12/9 00:00:00");
+  var start = new Date("2020/6/9 00:00:00");
 
   function update() {
     var now = new Date();
